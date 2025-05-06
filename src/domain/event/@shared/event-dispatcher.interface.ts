6382ff4caae04c1b453aa812c0401f0c0e50b011
marcos <msaorc@hotmail.com>
2025-05-06 10:13:27 -0300

    Adicionando testes do disparador de enventos

--- src/domain/event/@shared/event-dispatcher.interface.ts
+++ src/domain/event/@shared/event-dispatcher.interface.ts
@@ -4,4 +4,6 @@ import EventInterface from "./event.interface";
 export default interface EventDispatcherInterface {
     notify(event: EventInterface): void;
     register(eventName: string, eventHandler: EventHandlerInterface): void;
+    unregister(eventName: string, eventHandler: EventHandlerInterface): void;
+    unregisterAll(): void;
 }
