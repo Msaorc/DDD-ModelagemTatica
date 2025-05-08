c24cef260dc538c15f9b4fd47cf8aaea53cfb468
marcos <msaorc@hotmail.com>
2025-05-08 15:03:12 -0300

    Ajustando os handles de customer

--- src/domain/customer/event/handler/envia-console-log.handler.ts
+++ src/domain/customer/event/handler/envia-console-log.handler.ts
@@ -3,7 +3,7 @@ import EventInterface from "../../../@shared/event/event.interface";
 import AddressCustomerChangeEvent from "../address-customer-change.event";
 
 export default class EnviaConsoleLogHandler implements EventHandlerInterface<AddressCustomerChangeEvent>{
-    handle(event: EventInterface): void {
+    handle(event: AddressCustomerChangeEvent): void {
         console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData.Address.toString()}`);
     }
 }
--- src/domain/customer/event/handler/envia-console-log1-handler.ts
+++ src/domain/customer/event/handler/envia-console-log1-handler.ts
@@ -3,7 +3,7 @@ import EventInterface from "../../../@shared/event/event.interface";
 import CustomerCreatedEvent from "../customer-created.event";
 
 export default class EnviaConsoleLog1Handler implements EventHandlerInterface<CustomerCreatedEvent>{
-    handle(event: EventInterface): void {
+    handle(event: CustomerCreatedEvent): void {
         console.log("Esse é o primeiro console.log do evento: CustomerCreated");
     }
 }
